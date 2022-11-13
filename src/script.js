2015dccd2cc03ee84fe59e53170e64bdaae9ef58
helen <helenpsmahan@gmail.com>
2022-11-12 16:11:03 -0800

    Updated API calls
    weather description, temp conversion, wind speed

--- src/script.js
+++ src/script.js
@@ -41,51 +41,82 @@ function formattedDate() {
   return (now = `${day}, ${month} ${now.getDate()}, ${year} | ${hour}:${min}`);
 }
 
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 // change date selector
 let currentDate = document.querySelector("p.date");
 currentDate.innerHTML = formattedDate();
 
-function updateTemp(result) {
-  console.log(result);
-  let tempCurrent = Math.round(result.data.main.temp);
+let currentUnits = "metric";
+let currentCity = "San Diego";
+let windSpeedUnits = {imperial: " mph", metric: " km/h"};
+
+function updateValues(newCity, temps, wind, weather, units) {
+  let celciusLink = document.querySelector("#celcius-link");
+  let fahrenheitLink = document.querySelector("#fahrenheit-link");
+  if (units === "metric") {
+    fahrenheitLink.classList.add("inactive-degrees");
+    celciusLink.classList.remove("inactive-degrees");
+  } else {
+    fahrenheitLink.classList.remove("inactive-degrees");
+    celciusLink.classList.add("inactive-degrees");
+  }
+
+  let tempCurrent = Math.round(temps.temp);
   let tempMainHeader = document.querySelector("#todays-weather-temp");
   tempMainHeader.innerHTML = `${tempCurrent}°`;
 
-  let tempHigh = Math.round(result.data.main.temp_max);
+  let tempHigh = Math.round(temps.temp_max);
   let tempSidebarHigh = document.querySelector("#temp-high");
   tempSidebarHigh.innerHTML = tempHigh;
-  let tempLow = Math.round(result.data.main.temp_min);
+  let tempLow = Math.round(temps.temp_min);
   let tempSidebarLow = document.querySelector("#temp-low");
   tempSidebarLow.innerHTML = tempLow;
+
+  let windSpeedElement = document.querySelector("#wind-speed-value");
+  windSpeedElement.innerHTML = Math.round(wind.speed);
+  let windSpeedUnitsElement = document.querySelector("#wind-speed-units");
+  windSpeedUnitsElement.innerHTML = windSpeedUnits[units];
+
+  let descriptionElement = document.querySelector("#weather-description");
+  descriptionElement.innerHTML = capitalizeFirstLetter(weather[0].description);
+
+  let iconUrl = "http://openweathermap.org/img/w/" + weather[0].icon + ".png";
+  let weatherIconElement = document.querySelector("#weather-icon");
+  weatherIconElement.setAttribute("src", iconUrl);
+  weatherIconElement.setAttribute("alt", weather[0].description);
+
+  let currentCityHeader = document.querySelector("h2");
+  currentCityHeader.innerHTML = capitalizeFirstLetter(newCity);
+}
+
+function updateTempForCity(city, units) {
+  let apiKey = "281450ec88936f4fa8ee9864682b49a0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(({data: {main: temps, wind, weather}}) => {
+    currentUnits = units;
+    currentCity = city;
+    updateValues(city, temps, wind, weather, units);
+  });
 }
 
 // search engine
 function submitFormButton(event) {
   event.preventDefault();
-  let cityInput = document.querySelector("#city-input");
-  let currentCity = document.querySelector("h2");
-  currentCity.innerHTML = cityInput.value;
-
-  let apiKey = "281450ec88936f4fa8ee9864682b49a0";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(updateTemp);
+  updateTempForCity(document.querySelector("#city-input").value, currentUnits);
 }
 let submitForm = document.querySelector("#city-search");
 submitForm.addEventListener("submit", submitFormButton);
 
-// convert temperature
-// function switchTempF(event) {
-//   let currentTempF = document.querySelector("#todays-weather-temp");
-//   currentTempF.innerHTML = "23°";
-// }
+let celciusLink = document.querySelector("#celcius-link");
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+celciusLink.addEventListener("click", () => {
+  updateTempForCity(currentCity, "metric");
+});
+fahrenheitLink.addEventListener("click", () =>
+  updateTempForCity(currentCity, "imperial")
+);
 
-// let useFarenheit = document.querySelector("#farenheit");
-// useFarenheit.addEventListener("click", switchTempF);
-
-// function switchTempC(event) {
-//   let currentTempC = document.querySelector("#todays-weather-temp");
-//   currentTempC.innerHTML = "50°";
-// }
-// let useCelcius = document.querySelector("#celcius");
-// useCelcius.addEventListener("click", switchTempC);
+updateTempForCity("San Diego", "metric");
